6c7483d652dd0a824f5617fb13df39508dfe5ddb
LuckyCIover <30575765+LuckyCIover@users.noreply.github.com>
2019-04-16 15:37:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm> 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <complex>
 #include <string>
 #include <vector>
@@ -10,6 +11,7 @@
 #include <limits>
 #include <string>
 #include <time.h> 
+#include <windows.h>
 
 using namespace std;
 
@@ -66,8 +68,6 @@ vector <z_type> DFT(vector <z_type> inputVectorX)
 
 	vector <z_type> outputVectorY(N, 0);
 
-	clock_t start = clock();
-
 	for (size_t k = 0; k < N; k++)
 	{
 		z_type tempSum = 0;
@@ -79,11 +79,6 @@ vector <z_type> DFT(vector <z_type> inputVectorX)
 	}
 
 	clock_t end = clock();
-
-	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-
-	cout.precision(numeric_limits<double>::max_digits10 + 1);
-	cout << "Computing time: " << seconds << " seconds" << endl;
 
 	return outputVectorY;
 }
@@ -117,8 +112,6 @@ vector <z_type> FFT_wop(vector <z_type> inputVectorX)
 
 	z_type omega;
 
-	clock_t start = clock();
-
 	for (int k = 1; k < n + 1; k++)
 	{
 		int jLimit = (int)pow(2, k - 1);
@@ -138,13 +131,6 @@ vector <z_type> FFT_wop(vector <z_type> inputVectorX)
 
 		inputVectorX = outputVectorY;
 	}
-
-	clock_t end = clock();
-
-	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-
-	cout.precision(numeric_limits<double>::max_digits10 + 1);
-	cout << "Computing time: " << seconds << " seconds" << endl;
 
 	return outputVectorY;
 }
@@ -299,7 +285,7 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		cout << "Program usage template: ./dsp.exe operation inputfile1 inputfile2(for CONV/FCONV) outputfile" << endl;
+		cout << "Program usage template: .\\dsp.exe operation inputfile1 inputfile2(for CONV/FCONV) outputfile" << endl;
 		cout << "Operations:" << endl;
 		cout << "DFT IDFT FFT IFFT CONV FCONV" << endl;
 		return 1;
@@ -307,13 +293,13 @@ int main(int argc, char *argv[])
 	else
 	{
 		vector <z_type> X, Y;
-		vector <z_type> A, B, C;
+		vector <z_type> A, B, C, C1, C2;
 
 		if (string(argv[1]) == "DFT")
 		{
 			if (argc != 4)
 			{
-				cout << "Program usage template: ./dsp.exe DFT inputfile outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe DFT inputfile outputfile" << endl;
 				exit(1);
 			}
 			else
@@ -329,7 +315,7 @@ int main(int argc, char *argv[])
 		{
 			if (argc != 4)
 			{
-				cout << "Program usage template: ./dsp.exe IDFT inputfile outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe IDFT inputfile outputfile" << endl;
 				exit(1);
 			}
 			else
@@ -345,7 +331,7 @@ int main(int argc, char *argv[])
 		{
 			if (argc != 4)
 			{
-				cout << "Program usage template: ./dsp.exe FFT inputfile outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe FFT inputfile outputfile" << endl;
 				exit(1);
 			}
 			else
@@ -361,7 +347,7 @@ int main(int argc, char *argv[])
 		{
 			if (argc != 4)
 			{
-				cout << "Program usage template: ./dsp.exe IFFT inputfile outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe IFFT inputfile outputfile" << endl;
 				exit(1);
 			}
 			else
@@ -378,7 +364,7 @@ int main(int argc, char *argv[])
 			if (argc != 5)
 			{
 
-				cout << "Program usage template: ./dsp.exe CONV inputfile1 inputfile2 outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe CONV inputfile1 inputfile2 outputfile" << endl;
 				exit(1);
 			}
 			else
@@ -395,7 +381,7 @@ int main(int argc, char *argv[])
 		{
 			if (argc != 5)
 			{
-				cout << "Program usage template: ./dsp.exe FCONV inputfile1 inputfile2 outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe FCONV inputfile1 inputfile2 outputfile" << endl;
 				exit(1);
 			}
 			else
@@ -412,35 +398,172 @@ int main(int argc, char *argv[])
 		{
 			if (argc != 4)
 			{
-				cout << "Program usage template: ./dsp.exe DFT_FFT_TIME_TEST inputfile outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe DFT_FFT_TIME_TEST inputfile outputfile" << endl;
 				exit(1);
 			}
 			else
 			{
-				// Maksim, code here:
+				vector<z_type> Y_DFT, Y_FFT;
+				vector<double> DFT_time, FFT_time;
 
-				//
+				cout << "The results of the temporary comparison of DFT and FFT will appear below" << endl;
+				cout << "Please, wait..." << endl;
+
+				X = parseArray(argv[2]);
+
+				size_t N = X.size();
+				size_t n_max = (size_t)log2(N);
+				size_t N_current = N;
+
+				LARGE_INTEGER begin_DFT, end_DFT, begin_FFT, end_FFT, freq1, freq2;
+
+				cout << "---------------------------------------" << endl;
+
+				for (size_t n = 0; n < n_max; n++)
+				{
+					QueryPerformanceCounter(&begin_DFT);
+					Y_DFT = DFT(X);
+					QueryPerformanceCounter(&end_DFT);
+					QueryPerformanceFrequency(&freq1);
+
+					QueryPerformanceCounter(&begin_FFT);
+					Y_FFT = FFT_wop(X);
+					QueryPerformanceCounter(&end_FFT);
+					QueryPerformanceFrequency(&freq2);
+
+					double seconds_DFT = (double)(end_DFT.QuadPart - begin_DFT.QuadPart) / freq1.QuadPart;
+					double seconds_FFT = (double)(end_FFT.QuadPart - begin_FFT.QuadPart) / freq2.QuadPart;
+
+					cout.precision(numeric_limits<double>::max_digits10 + 1);
+					cout << "N: " << N_current << endl;
+					cout << "DFT_time: " << seconds_DFT << " seconds" << endl;
+					cout << "FFT_time: " << seconds_FFT << " seconds" << endl;
+					cout << "---------------------------------------" << endl;
+
+					DFT_time.insert(DFT_time.begin(), seconds_DFT);
+					FFT_time.insert(FFT_time.begin(), seconds_FFT);
+
+					N_current = N_current / 2;
+
+					X.resize(N_current);
+				}
+
+				ofstream outputFile(argv[3]);
+
+				if (outputFile.is_open())
+				{
+					outputFile.precision(5);
+					outputFile.setf(ios::fixed);
+					for (size_t n = 0; n < n_max; n++)
+						outputFile << DFT_time[n] << " " << FFT_time[n] << endl;
+				}
+				else
+				{
+					cout << "Error opening output file" << endl;
+					exit(1);
+				}
+
+				outputFile.close();
 			}
 		}
 
 		if (string(argv[1]) == "CONV_FCONV_TIME_TEST")
 		{
-			if (argc != 5)
+			if (argc != 6)
 			{
-				cout << "Program usage template: ./dsp.exe CONV_FCONV_TIME_TEST inputfile1 inputfile2 outputfile" << endl;
+				cout << "Program usage template: .\\dsp.exe CONV_FCONV_TIME_TEST 0/1(both change size) inputfile1 inputfile2 outputfile" << endl;
 				exit(1);
 			}
 			else
 			{
-				// Maksim, code here:
+				vector<double> CONV_time, FCONV_time;
 
-				//
+				cout << "The results of the temporary comparison of DFT and FFT will appear below" << endl;
+				cout << "Please, wait..." << endl;
+
+				A = parseArray(argv[3]);
+				B = parseArray(argv[4]);
+
+				size_t L = A.size();
+				size_t M = B.size();
+				size_t L_current = L;
+				size_t M_current;
+				double norm = 0.0;
+
+				LARGE_INTEGER begin_СONV, end_CONV, begin_FCONV, end_FCONV, freq1, freq2;
+
+				if (atoi(argv[2]) == 1)
+					cout << "Both 1st and 2nd are changing size: " << endl;
+				else
+					cout << "1st array is fixed, 2nd is changing size: " << endl;
+
+				cout << "---------------------------------------" << endl;
+
+				for (M_current = M; M_current > M / 10; M_current -= M / 10)
+				{
+					QueryPerformanceCounter(&begin_СONV);
+					C1 = directConvolution(A, B);
+					QueryPerformanceCounter(&end_CONV);
+					QueryPerformanceFrequency(&freq1);
+
+					QueryPerformanceCounter(&begin_FCONV);
+					C2 = FFTConvolution(A, B);
+					QueryPerformanceCounter(&end_FCONV);
+					QueryPerformanceFrequency(&freq2);
+
+					for (size_t k = 0; k < C1.size(); k++)
+					{
+						if (norm < abs(C2[k] - C1[k]))
+							norm = abs(C2[k] - C1[k]);
+					}
+
+					double seconds_CONV = (double)(end_CONV.QuadPart - begin_СONV.QuadPart) / freq1.QuadPart;
+					double seconds_FCONV = (double)(end_FCONV.QuadPart - begin_FCONV.QuadPart) / freq2.QuadPart;
+
+					cout.precision(numeric_limits<double>::max_digits10 + 1);
+					cout << "M: " << M_current << endl;
+					cout << "Err_norm: " << norm << endl;
+					cout << "CONV_time: "  << seconds_CONV << " seconds" << endl;
+					cout << "FCONV_time: " << seconds_FCONV << " seconds" << endl;
+					cout << "---------------------------------------" << endl;
+
+					CONV_time.insert(CONV_time.begin(), seconds_CONV);
+					FCONV_time.insert(FCONV_time.begin(), seconds_FCONV);
+
+					if (atoi(argv[2]) == 1)
+					{
+						L_current = L_current - L / 10;
+						A.resize(L_current);
+					}
+
+					B.resize(M_current);
+				}
+
+
+				ofstream outputFile(argv[5]);
+
+				if (outputFile.is_open())
+				{
+					outputFile.precision(5);
+					outputFile.setf(ios::fixed);
+				
+
+					for (size_t m = 0; m < CONV_time.size(); m++)
+						outputFile << M_current + (m + 1) * (M / 10) << " " << CONV_time[m] << " " << FCONV_time[m] << endl;
+				}
+				else
+				{
+					cout << "Error opening output file" << endl;
+					exit(1);
+				}
+
+				outputFile.close();
 			}
 		}
 
 		cout << "Check your result in the output file" << endl;
 	}
 
+	system("pause");
 	return 0;
 }
-
